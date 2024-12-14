6bac3a62c49ceacb1ae30a4542e3189e420f5554
Aahilmalik85 <ahmedishtiaquemalik@gmail.com>
2024-12-14 21:45:52 +0500

    first commit

---
+++ my-app/app/Components/Share.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import React from 'react'
+
+
+
+export default function Share() {
+  return (
+    <div className=''>
+    <h1>Share your setup with</h1>
+<Image
+src="/Title.png"
+alt="Title"
+height={}
+className="rounded-lg"
+/>
+
+    </div>
+  );
+}
